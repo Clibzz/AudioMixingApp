f1eb8a4d41deb61005a177eca06c4f4edb64c9d5
Chris <chriscrossje2001@gmail.com>
2023-10-22 20:15:08 +0200

    Added filterpage front-end

---
+++ AudioMixingApp/AudioMixingApp/FilterPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace AudioMixingApp;
+
+public partial class FilterPage : ContentPage
+{
+	public FilterPage()
+	{
+		InitializeComponent();
+	}
+}
--- AudioMixingApp/AudioMixingApp/MainPage.xaml.cs
+++ AudioMixingApp/AudioMixingApp/MainPage.xaml.cs
@@ -8,17 +8,10 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void FilterPageButton_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new FilterPage());
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
