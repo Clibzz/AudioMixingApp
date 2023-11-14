5d1a112c736faf31211d4f0f0eed46a73c7c2c80
Yannieck Blaauw <91061289+Yannieck@users.noreply.github.com>
2023-11-14 17:05:46 +0100

    Added main code for equalizer
    Working on removing BiQuadFilter

--- AudioMixingApp/AudioMixingApp/App.xaml.cs
+++ AudioMixingApp/AudioMixingApp/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace AudioMixingApp;
+﻿using AudioMixingApp.Effects;
+using NAudio.Wave;
+
+namespace AudioMixingApp;
 using Views;
 
 public partial class App : Application
@@ -6,6 +9,18 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
+		
+		string inputFilePath =
+			"C:/Users/Yanni/Downloads/@Pantera-Cowboys-from-Hell.mp3"; // Replace with your input audio file path
+
+		ISampleProvider audioFile = new AudioFileReader(inputFilePath);
+        
+		Equalizer equalizer = new Equalizer(audioFile);
+  
+		WaveOutEvent waveOut = new WaveOutEvent();
+		waveOut.Init(equalizer);
+		waveOut.Play();
+		waveOut.Volume = 0.5f;
 
 		MainPage = new NavigationPage(new MainPage());
 	}
--- AudioMixingApp/AudioMixingApp/Effects/Equalizer.cs
+++ AudioMixingApp/AudioMixingApp/Effects/Equalizer.cs
@@ -1,64 +1,59 @@
-using System.Diagnostics;
+using NAudio.Dsp;
 using NAudio.Wave;
 
 namespace AudioMixingApp.Effects;
 
 public class Equalizer : ISampleProvider
 {
-    private readonly ISampleProvider source;
-    private readonly int windowSize;
-    private readonly int channels;
-    private readonly float[] buffer;
+    private readonly ISampleProvider _source;
 
-    public WaveFormat WaveFormat => source.WaveFormat;
+    private readonly BiQuadFilter _lowFilter;
+    private readonly BiQuadFilter _midFilter;
+    private readonly BiQuadFilter _highFilter;
 
-    public Equalizer(ISampleProvider source, int cutoffFrequency)
+    public Equalizer(ISampleProvider source)
     {
-        this.source = source;
-        this.channels = source.WaveFormat.Channels;
-
-        // Calculate the number of samples for the moving average window
-        int sampleRate = source.WaveFormat.SampleRate;
-        this.windowSize = (int)(sampleRate / (float)cutoffFrequency);
-
-        this.buffer = new float[windowSize * channels];
+        _source = source;
+        
+        // Set up the initial filter parameters
+        _lowFilter = BiQuadFilter.PeakingEQ(source.WaveFormat.SampleRate, 100, 1, 0);
+        _midFilter = BiQuadFilter.PeakingEQ(source.WaveFormat.SampleRate, 1000, 1, 0);
+        _highFilter = BiQuadFilter.PeakingEQ(source.WaveFormat.SampleRate, 5000, 1, 0);
     }
 
     public int Read(float[] buffer, int offset, int count)
     {
-        int samplesRead = source.Read(buffer, offset, count);
+        int samplesRead = _source.Read(buffer, offset, count);
 
-        for (int i = 0; i < samplesRead; i += channels)
-        {
-            ApplyMovingAverage(buffer, offset + i, channels);
-        }
+        Process(buffer, offset, samplesRead);
 
         return samplesRead;
     }
 
-    private void ApplyMovingAverage(float[] buffer, int offset, int channels)
+    public WaveFormat WaveFormat => _source.WaveFormat;
+
+    private void Process(float[] buffer, int offset, int count)
     {
-        for (int c = 0; c < channels; c++)
+        for (int i = 0; i < count; i++)
         {
-            float sum = 0.0f;
-
-            // Calculate the average of the samples in the window
-            for (int i = offset - windowSize * channels; i <= offset; i += channels)
-            {
-                if (i >= 0)
-                {
-                    sum += buffer[i + c];
-                }
-            }
-
-            // Update the current sample with the moving average
-            this.buffer[c] = sum / windowSize;
+            buffer[i + offset] = _lowFilter.Transform(buffer[i + offset]);
+            buffer[i + offset] = _midFilter.Transform(buffer[i + offset]);
+            buffer[i + offset] = _highFilter.Transform(buffer[i + offset]);
         }
+    }
 
-        // Copy the filtered samples back to the original buffer
-        for (int c = 0; c < channels; c++)
-        {
-            buffer[offset + c] = this.buffer[c];
-        }
+    public void SetLows(int value)
+    {
+        _lowFilter.SetPeakingEq(_source.WaveFormat.SampleRate, 100, 1, value);
+    }
+
+    public void SetMids(int value)
+    {
+        _lowFilter.SetPeakingEq(_source.WaveFormat.SampleRate, 1000, 1, value);
+    }
+
+    public void SetHighs(int value)
+    {
+        _lowFilter.SetPeakingEq(_source.WaveFormat.SampleRate, 5000, 1, value);
     }
 }
