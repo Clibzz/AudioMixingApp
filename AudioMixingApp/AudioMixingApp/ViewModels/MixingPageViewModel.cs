5cadd16a00d8d8dee7be622f2b040dd9078e7c47
Yannieck Blaauw <91061289+Yannieck@users.noreply.github.com>
2023-11-15 12:59:15 +0100

    Tried adding auto updating on the front end

--- AudioMixingApp/AudioMixingApp/ViewModels/MixingPageViewModel.cs
+++ AudioMixingApp/AudioMixingApp/ViewModels/MixingPageViewModel.cs
@@ -1,4 +1,6 @@
+using System.ComponentModel;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using AudioMixingApp.Effects;
 using AudioMixingApp.Models;
 using NAudio.Wave;
@@ -8,8 +10,9 @@ namespace AudioMixingApp.ViewModels;
 public class MixingPageViewModel
 {
     public List<Song> Songs { get; set; }
-    
-    public float CurrentValue { get; set; }
+
+    // public int CurrentValue { get; set; }
+    public int SongDuration { get; set; }
 
     private readonly System.Timers.Timer _timer;
     private WaveOutEvent waveOut;
@@ -19,7 +22,7 @@ public class MixingPageViewModel
         _timer = new System.Timers.Timer();
         _timer.Interval = 1000;
         _timer.Start();
-        
+
         waveOut = new WaveOutEvent();
     }
 
@@ -30,17 +33,46 @@ public class MixingPageViewModel
             waveOut.Stop();
             return;
         }
-        
+
         AudioFileReader audioFile = new AudioFileReader(path);
-        
+
         waveOut.Init(audioFile);
         waveOut.Play();
         waveOut.Volume = volume;
-        
+
+        SongDuration = (int)audioFile.TotalTime.TotalSeconds;
+
         _timer.Elapsed += (sender, eventArgs) =>
         {
-            CurrentValue = audioFile.CurrentTime.Seconds;
-            Trace.WriteLine(waveOut.PlaybackState);
+            CurrentValue = (int)audioFile.CurrentTime.TotalSeconds;
+            Trace.WriteLine(CurrentValue);
+            Trace.WriteLine(SongDuration);
         };
+    }
+    
+    
+    
+    
+    
+    
+    private int _CurrentValue = 10;
+    public int CurrentValue
+    {
+        get => _CurrentValue;
+        set
+        {
+            if (_CurrentValue != value)
+            {
+                _CurrentValue = value;
+                OnPropertyChanged("CurrentValue"); 
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    
+    private void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
