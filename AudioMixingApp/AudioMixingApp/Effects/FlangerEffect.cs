ce9ef99771b343fd1b6a633a552609200d65fbc2
Monique <89770769+Moniquesbg@users.noreply.github.com>
2023-11-19 19:44:34 +0100

    Adjusted read function of flanger with modulation

--- AudioMixingApp/AudioMixingApp/Effects/FlangerEffect.cs
+++ AudioMixingApp/AudioMixingApp/Effects/FlangerEffect.cs
@@ -1,68 +1,63 @@
 ﻿using NAudio.Wave;
+using System;
 
-namespace AudioMixingApp.Effects
+public class FlangerEffect : ISampleProvider
 {
-    class FlangerEffect : ISampleProvider
+    private readonly ISampleProvider source;
+    private readonly int delayMilliseconds;
+    private readonly float feedback;
+    private readonly int sampleRate;
+    private readonly float modulationFrequency; // Frequency of the modulation LFO
+
+    private int delaySamples;
+    private float[] delayBuffer;
+    private int delayBufferPosition;
+
+    // Extra field to store the current flanger factor
+    private float flangerFactor;
+
+    public FlangerEffect(ISampleProvider source, int delayMilliseconds, float feedback, int sampleRate, float modulationFrequency)
     {
-        //The song that gets imported 
-        private ISampleProvider _song { get; set; }
+        this.source = source;
+        this.delayMilliseconds = delayMilliseconds;
+        this.feedback = feedback;
+        this.sampleRate = sampleRate;
+        this.modulationFrequency = modulationFrequency;
 
-        private float[] _flangerBuffer { get; set; }
-        private int _flangerBufferPosition { get; set; }
-        private float _flangerFactor { get; set; }
-        public WaveFormat WaveFormat { get { return _song.WaveFormat; } }
-       
-        public FlangerEffect(ISampleProvider source, float flangerFactor)
-        {
-            //The song
-            _song = source;
-            //Factor to change the flanger effect
-            _flangerFactor = flangerFactor;
-            //Adjust the length of the Flanger buffer 
-            int flangerBufferLength = (int)(WaveFormat.SampleRate * 0.02); 
-            _flangerBuffer = new float[flangerBufferLength];
+        this.delaySamples = (int)((delayMilliseconds / 1000.0) * sampleRate);
+        this.delayBuffer = new float[delaySamples];
+        this.delayBufferPosition = 0;
 
-            _flangerBufferPosition = 0;
-        }
-        /// <summary>
-        /// Method IncreaseFlanger() increases the flanger effect.
-        /// The maximum is 0.5
-        /// </summary>
-        public void IncreaseFlanger()
-        {
-            _flangerFactor = (float)Math.Round(Math.Min(_flangerFactor + 0.1, 0.5), 1); 
-        }
-
-        /// <summary>
-        /// Method DecreaseFlanger(): decreases the flanger effect by changing the factor
-        /// </summary>
-        public void DecreaseFlanger()
-        {
-            _flangerFactor = (float)Math.Round(Math.Max(_flangerFactor - 0.1, 0.0), 1); 
-        }
-
-        /// <summary>
-        /// This function reads the song 
-        /// </summary>
-        /// <param name="buffer">The song that is going to be played</param>
-        /// <param name="offset">The position from where the samples in the buffer will be read</param>
-        /// <param name="count">The amount of audio samples that will be read</param>
-        /// <returns>The amount of read samples</returns>
-        public int Read(float[] buffer, int offset, int count)
-        {
-            int readSamples = _song.Read(buffer, offset, count);
-           
-            for (int i = 0; i < readSamples; i++)
-            {
-                buffer[i] += _flangerBuffer[_flangerBufferPosition] * _flangerFactor;
-
-                _flangerBuffer[_flangerBufferPosition] = buffer[i] + _flangerBuffer[_flangerBufferPosition] * 0.5f;
-                _flangerBufferPosition++;
-
-                if (_flangerBufferPosition == _flangerBuffer.Length)
-                    _flangerBufferPosition = 0;
-            }
-            return readSamples;
-        }
+        // Initialize flanger factor 
+        this.flangerFactor = 0.5f; 
     }
+
+    public int Read(float[] buffer, int offset, int count)
+    {
+        int samplesRead = source.Read(buffer, offset, count);
+
+        for (int n = 0; n < samplesRead; n++)
+        {
+            float input = buffer[offset + n];
+            float modulation = (float)Math.Sin(2 * Math.PI * modulationFrequency * n / sampleRate); 
+
+          
+            int modulatedDelaySamples = delaySamples + (int)(modulation * delaySamples / 2 * flangerFactor);
+            int delayIndex = (delayBufferPosition + modulatedDelaySamples) % delaySamples;
+
+            float delayedSample = delayBuffer[delayIndex];
+            delayBuffer[delayBufferPosition] = input + delayedSample * feedback;
+            buffer[offset + n] += delayedSample;
+
+            delayBufferPosition++;
+            if (delayBufferPosition == delaySamples)
+            {
+                delayBufferPosition = 0;
+            }
+        }
+
+        return samplesRead;
+    }
+
+    public WaveFormat WaveFormat => source.WaveFormat;
 }
