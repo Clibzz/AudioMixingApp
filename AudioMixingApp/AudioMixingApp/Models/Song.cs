16c8829e4268eb495041afb3a2128bc281fa0b7a
VictorP03 <crapowner321@gmail.com>
2023-11-21 20:42:36 +0100

    Removed duration and effects from song model.

--- AudioMixingApp/AudioMixingApp/Models/Song.cs
+++ AudioMixingApp/AudioMixingApp/Models/Song.cs
@@ -2,8 +2,6 @@ namespace AudioMixingApp.Models;
 
 public class Song
 {
-    public List<Effect> Effects { get; set; }
     public string Title { get; set; }
     public string Artist { get; set; }
-    public string Duration {  get; set; }   
 }
