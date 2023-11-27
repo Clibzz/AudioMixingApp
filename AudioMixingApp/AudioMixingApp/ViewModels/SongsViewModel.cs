01c5cf45f15e53ea2d9727436e728e9b5b53868b
Monique <89770769+Moniquesbg@users.noreply.github.com>
2023-11-27 15:19:28 +0100

    Able to get songs from json file and display it in overview

--- AudioMixingApp/AudioMixingApp/ViewModels/SongsViewModel.cs
+++ AudioMixingApp/AudioMixingApp/ViewModels/SongsViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using AudioMixingApp.Models;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using static AudioMixingApp.Views.SongsPage;
+using System.Text.Json;
 
 namespace AudioMixingApp.ViewModels
 {
     public class SongsViewModel
     {
         ObservableCollection<Song> songs;
+        string jsonPath = $@"C:\Users\{Environment.UserName}\Documents\AudioMixingApp\songs.json";
+
         public ObservableCollection<Song> Songs
         {
             get { return songs; }
@@ -30,11 +34,47 @@ namespace AudioMixingApp.ViewModels
 
         public SongsViewModel()
         {
-            Songs = new ObservableCollection<Song>
+            Songs = LoadSongsFromJsonFile(jsonPath);
+        }
+
+        private ObservableCollection<Song> LoadSongsFromJsonFile(string jsonFilePath)
+        {
+            ObservableCollection<Song> loadedSongs = new ObservableCollection<Song>();
+
+            try
             {
-                new Song { Title = "Cowboys from hell", Artist = "Pantera" },
-                new Song { Title = "Bleed", Artist = "Meshuggah" },
-            };
+                if (File.Exists(jsonPath))
+                {
+                    string jsonContent = File.ReadAllText(jsonPath);
+
+                    // Deserialize the JSON content into a class representing your JSON structure
+                    var songListWrapper = JsonSerializer.Deserialize<SongListWrapper>(jsonContent);
+
+                    if (songListWrapper != null)
+                    {
+                        foreach (var song in songListWrapper.Songs)
+                        {
+                            // Create a new Song instance and set its properties
+                            var newSong = new Song
+                            {
+                                Title = song.Title,
+                                Artist = song.Artist,
+                                FilePath = song.FilePath
+                            };
+
+                            loadedSongs.Add(newSong);
+                        }
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., file not found, JSON parsing errors)
+                Console.WriteLine($"Error loading songs from JSON file: {ex.Message}");
+            }
+
+            return loadedSongs;
         }
     }
 }
