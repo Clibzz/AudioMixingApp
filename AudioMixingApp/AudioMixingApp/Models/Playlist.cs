9fd2058849569bb86d8128e520a109851c9a43ad
Clibzz <chriscrossje2001@gmail.com>
2023-11-30 11:51:33 +0100

    Bug fix binding errors

--- AudioMixingApp/AudioMixingApp/Models/Playlist.cs
+++ AudioMixingApp/AudioMixingApp/Models/Playlist.cs
@@ -3,6 +3,8 @@
     internal class Playlist
     {
         public string Name { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
         public List<Song> Songs { get; set;} = new List<Song>();
     }
 }
